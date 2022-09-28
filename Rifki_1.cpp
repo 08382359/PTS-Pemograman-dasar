914c9178408e15a7a95f5411c6d73531befc0ab2
Rifki ramdani27 <111963680+08382359@users.noreply.github.com>
2022-09-28 15:38:29 +0700

    Add files via upload

---
+++ Rifki_1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+//Compiler version g++ 6.3.0
+
+int main()
+{
+   cout << "Nama:Rifki ramdani"<<endl;
+  cout << "kelas:X RPL 1"<<endl;
+ cout << "nilai:95"<<endl;
+ }
+ 
